3478d4fd55b60d2a15e98640699f5eb27da90dad
Eugene Pavlov <eugenepavlov@Eugenes-MacBook-Pro.local>
2016-12-15 20:56:21 -0500

    Created data model

---
+++ Wisher/ItemType+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  ItemType+CoreDataClass.swift
+//  Wisher
+//
+//  Created by Eugene Pavlov on 12/15/16.
+//  Copyright © 2016 Eugene Pavlov. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(ItemType)
+public class ItemType: NSManagedObject {
+
+}
