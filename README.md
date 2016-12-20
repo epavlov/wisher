028ea65e1b78666b8d56c9f04fce721cca8ae6f6
Eugene Pavlov <eugenepavlov@Eugenes-MacBook-Pro.local>
2016-12-19 21:45:18 -0500

    Added screenshot and description to a project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # wisher
-Wishlist app
+Simple Wishlist app
+
+Shows how to create data model and do CRUD operations with CoreData as well as use UITableView, UINavigationViewController, UIImagePickerView, UIPickerView, work with text field styles, etc.
+
+Written in Swift 3 and xCode Version 8.2 (8C38)
+
+![alt tag](https://raw.githubusercontent.com/epavlov/wisher/master/screenshot_1.PNG)
+![alt tag](https://raw.githubusercontent.com/epavlov/wisher/master/screenshot_2.PNG)
--- Wisher/MainVC.swift
+++ Wisher/MainVC.swift
@@ -84,8 +84,19 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
      */
     func attemptFetch() {
         let fetchRequest: NSFetchRequest<Item> = Item.fetchRequest()
+        
+        // Adding sorting functionality
         let dateSort = NSSortDescriptor(key: "created", ascending: false)
-        fetchRequest.sortDescriptors = [dateSort]
+        let priceSort = NSSortDescriptor(key: "price", ascending: true)
+        let titleSort = NSSortDescriptor(key: "title", ascending: true)
+        
+        if segment.selectedSegmentIndex == 0 {
+            fetchRequest.sortDescriptors = [dateSort]
+        } else if segment.selectedSegmentIndex == 1 {
+            fetchRequest.sortDescriptors = [priceSort]
+        } else if segment.selectedSegmentIndex == 2 {
+            fetchRequest.sortDescriptors = [titleSort]
+        }
         
         let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
         
@@ -98,6 +109,14 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
             let error = error as NSError
             print("\(error.debugDescription)")
         }
+    }
+    
+    /**
+     Function to change sorting on user action
+     */
+    @IBAction func segmentChanged(_ sender: Any) {
+        attemptFetch()
+        tableView.reloadData()
     }
     
     func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
